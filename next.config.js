467f1e2311af17ed399e80f19a78126ad5e05f76
mrlectus <lilbrown3000@gmail.com>
2024-12-05 15:21:16 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverExternalPackages: ["argon2"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
